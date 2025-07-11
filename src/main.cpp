59d08a284567d30b7fba01e836ebc911b44963e4
rkc137 <hlopov100@gmail.com>
2025-07-11 17:34:13 +0300

    embeded resources

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,11 @@
 #include <filesystem>
 #include <regex>
 
-#include <string_literal.hpp>
+#include <rkc/string_literal.hpp>
+
+#include "battery/embed.hpp"
 
 namespace fs = std::filesystem;
-
-std::string read_resourse(fs::path &&file)
-{
-    std::ifstream ifs(file);
-    using It = std::istreambuf_iterator<char>;
-    return std::string(It(ifs), It());
-}
 
 template <rkc::string_literal_t msg>
 void exit_if(bool expr)
@@ -21,8 +16,8 @@ void exit_if(bool expr)
     if(!expr) return; 
     // i dont care if you cant grep this message btw
     std::cout << msg << "\nusage: csetup\n"
-        << "\t- -f, --folder <path>: Specify the folder path for the project. Must not already exist.\n"
-        << "\t- -n, --name <name>: Specify the project name (must be at least 1 character long). \n";
+        << "\t\t-f, --folder <path>: Specify the folder path for the project. Must not already exist.\n"
+        << "\t\t-n, --name <name>: Specify the project name (must be at least 1 character long). \n";
     std::exit(0);
 };
 
@@ -31,16 +26,6 @@ int main(int argc, char *argv[])
     std::optional<std::string> input_project_name;
     std::optional<std::string> cxx_standart;
     std::optional<fs::path> input_project_path;
-
-    auto templates_path = [&]() {
-        std::string path(argv[0]);
-        #ifdef _WIN32
-            char slash = '\\';
-        #elif __unix__
-            char slash = '/';
-        #endif
-        return fs::path{path.begin(), path.begin() + path.find_last_of(slash)} / "templates";
-    }();
 
     for(int i = 1; i < argc; i++)
     {
@@ -71,26 +56,36 @@ int main(int argc, char *argv[])
     }();
     if(!fs::create_directory(project_path))
         throw std::runtime_error("cannot create " + project_path.string());
-
+                
+    //  cmake
     std::ofstream cmake_file(project_path / "CMakeLists.txt"); 
     if(!cmake_file.is_open())
         throw std::runtime_error("cannot make cmake file");
-
-    std::string cmake_lists = read_resourse(templates_path / "CMakeLists.template");
+    std::string cmake_lists =  b::embed<"res/templates/CMakeLists.txt">();
     std::string standard = "23";
     cmake_lists = std::regex_replace(cmake_lists, std::regex("CSETUP_NAME"), project_name);
     cmake_lists = std::regex_replace(cmake_lists, std::regex("CSETUP_STANDARD"), standard);
-    cmake_file << cmake_lists;
     
+    // gitignore
     std::ofstream git_ignore_file(project_path / ".gitignore");
     if(!git_ignore_file.is_open())
         throw std::runtime_error("cannot make gitignore file");
-    git_ignore_file << read_resourse(templates_path / "gitignore.template");
+    auto git_ignore = b::embed<"res/templates/.gitignore">().str();
 
+    // source
     if(!fs::create_directory(project_path / "src"))
         throw std::runtime_error("cannot create folder");
     std::ofstream main_cpp_file(project_path / "src/main.cpp");
-    main_cpp_file << read_resourse(templates_path / "main.template");
+    auto main_cpp = b::embed<"res/templates/main.cpp">().str();
+
+    
+    // clear all that \r\n bullshit
+    for(auto s : {&cmake_lists, &git_ignore, &main_cpp})
+        s->erase(std::remove(s->begin(), s->end(), '\r'), s->end());
+    
+    cmake_file.write(cmake_lists.c_str(), cmake_lists.size());
+    git_ignore_file.write(git_ignore.c_str(), git_ignore.size());
+    main_cpp_file.write(main_cpp.c_str(), main_cpp.size());
     
     return 0;
 }
